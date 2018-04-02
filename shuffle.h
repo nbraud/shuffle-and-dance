ecf38888d20448ac3023c7e3d1134e927823ebec
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2018-04-02 19:23:18 +0200

    Add API header for shuffle.c

---
+++ shuffle.h
@@ -0,0 +1,10 @@
+// Bog-standard Fisher-Yates shuffle
+void shuffle_std(struct rand_t *r, int t[], size_t n);
+
+// Fisher-Yates shuffle with pre-generated indexes
+void shuffle_ind(int t[], size_t u[], size_t n);
+// Index table generation
+size_t *shuffle_gen(struct rand_t *r, size_t n);
+
+// Fisher-Yates shuffle with larger interleaved batches
+void shuffle_batch(struct rand_t *r, int t[], size_t n);
